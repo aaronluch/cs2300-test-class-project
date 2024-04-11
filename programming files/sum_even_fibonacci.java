c5d4d698f4030af467c0cef7ea73d1b9c663d892
lilybonds <143442728+lilybonds@users.noreply.github.com>
2024-04-11 10:42:31 -0400

    1 bug

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,10 +1,11 @@
+bugs introduced: LB
 public class sum_even_fibonacci {
     public static void main(String[] args) {
         int a = 0, b = 1;
         int sumEven = 0;
         while (b < 4000000) {
             if (b % 2 == 0) {
-                sumEven += b;
+                sumEven -= b;
             }
             int temp = b;
             b = a + b;
